6d8f493ec094a749e6830a3eb19bccc2571737d5
Elizabeth Nguyen <saper209@yahoo.com>
2022-05-12 21:01:54 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -21,3 +21,62 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(employee){
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees){
+    const record = []
+    employees.map(employee => record.push(createEmployeeRecord(employee)))
+    return record
+}
+
+function createTimeInEvent(dateStamp){
+    const [date, hour] = dateStamp.split(" ")
+    const timeIn = {
+        "type": "TimeIn",
+        "hour": parseInt(hour),
+        "date": date
+    }
+
+    this.timeInEvents.push(timeIn)
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    const [date, hour] = dateStamp.split(" ")
+    const timeOut = {
+        "type": "TimeOut",
+        "hour": parseInt(hour),
+        "date": date
+    }
+
+    this.timeOutEvents.push(timeOut)
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    const timeIn = this.timeInEvents.find(day => date === day.date)
+    const timeOut = this.timeOutEvents.find(day => date === day.date)
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(date){
+    return this.payPerHour * hoursWorkedOnDate.call(this, date)
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(employee => employee.firstName === firstName)
+}
+
+function calculatePayroll(employeeRecord){
+    const employeesPay = employeeRecord.map(employee => allWagesFor.call(employee))
+    return employeesPay.reduce((current, initial) => current + initial)
+}
